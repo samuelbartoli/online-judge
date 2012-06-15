1ca11cf803d1b7170fadf5890cd1d05dec5355ce
Samuel Bártoli <smbartoli@gmail.com>
2012-06-15 14:19:09 -0430

    agregado esqueleto de la aplicacion

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OnlineJudge::Application.config.secret_token = '99db94db8dd2d36e51419c2a393761bd256c7f07ca5e078c3b36d41d0721632efb38f686390d33c99c07ea59b06b101714bf0c40140162dc066080cf2807f1b3'
